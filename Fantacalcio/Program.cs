b5c2df13417f6fce8718017ddcac4f086593dd25
lucapellielo04 <luca.pellielo@iisviolamarchesini.edu.it>
2021-11-11 08:50:08 +0100

    Creazione progetto

---
+++ Fantacalcio/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fantacalcio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
